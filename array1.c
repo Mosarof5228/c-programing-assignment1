64f80fd8acfc6fb592450f71d45a4738353e82e2
unknown <mosarofhossen5228@gmail.com>
2024-03-21 14:22:57 +0600

    largest number

--- array1.c
+++ array1.c
@@ -10,20 +10,26 @@ int main(){
 // }
 
 
- int ages[]={20,22,18,35,48,26,87,70,1};
+ int ages[]={20,22,18,100,35,48,26,87,70,1};
  int size=sizeof(ages);
 //  printf("size of ages is: %d\n",size);
  int length=sizeof(ages)/sizeof(ages[0]);
 //  printf("length is : %d",length);
 int lowesetAge=ages[0];
+
+// for(int i=0;i<length;i++){
+//     if(lowesetAge>ages[i]){
+//         lowesetAge=ages[i];
+//     }
+// }
+// printf("lowest age is: %d",lowesetAge);
+int highestAge=ages[0];
 for(int i=0;i<length;i++){
-    if(lowesetAge>ages[i]){
-        lowesetAge=ages[i];
+    if(highestAge<ages[i]){
+        highestAge=ages[i];
     }
 }
-printf("lowest age is: %d",lowesetAge);
-
-
+printf("highest ages is: %d",highestAge);
 
 
 }
